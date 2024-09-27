2b93f7afeec3dcb9cda6fcf15358f80f1be07801
Pedro Lucas Pedro <pedro.lucs0089@gmail.com>
2024-09-27 17:12:58 -0300

    add success component

---
+++ src/components/is-subscriber.tsx
@@ -0,0 +1,28 @@
+import { MoveUp } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function IsSubscriber() {
+  return(
+    <div className="py-4 px-5 md:py-6 md:px-7 space-y-5 rounded-md border shadow-md bg-zinc-50">
+      <div>
+        <h1 className="text-2xl font-bold leading-none">Formulário preenchido com sucesso!</h1>
+        <p className="w-full max-w-80 text-muted-foreground">Entre em contato com o líder do seu departamento caso deseja alterar algo!</p>
+      </div>
+    
+      <Button 
+        variant="destructive" 
+        className="mt-5 gap-1"
+        onClick={() => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+          })
+        }}
+      >
+        Voltar ao topo
+
+        <MoveUp className="size-4"/>
+      </Button>
+    </div>
+  );
+}
--- src/components/subscribe.tsx
+++ src/components/subscribe.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import Cookies from "js-cookie";
-
 import { z } from "zod";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
@@ -23,6 +21,8 @@ import {
 } from "./ui/form";
 import { toast } from "./ui/use-toast";
 import { isPast } from "date-fns";
+import { useEffect, useState } from "react";
+import { IsSubscriber } from "./is-subscriber";
 import { useCreateSubscribeMutation } from "@/graphql/generated";
 
 const formSchema = z.object({
@@ -44,6 +44,15 @@ type FormTypes = z.infer<typeof formSchema>;
 export function Subscribe() {
 
   const [createSubscribe, { loading }] = useCreateSubscribeMutation();
+  const [isSubscriber, setIsSubscriber] = useState(false);
+
+  useEffect(() => {
+    const getSubscriberInStorage = localStorage.getItem("isSubscriber");
+
+    if(getSubscriberInStorage) {
+      setIsSubscriber(true);
+    }
+  }, []);
 
   const form = useForm<FormTypes>({
     resolver: zodResolver(formSchema)
@@ -79,7 +88,11 @@ export function Subscribe() {
         title: "Sua inscrição foi registrada!"
       });
 
-      setTimeout(() =>  window.location.reload(), 1700)
+      setIsSubscriber(true);
+      localStorage.setItem("isSubscriber", JSON.stringify({
+        date: new Date("2024-10-26")
+      }));
+      // setTimeout(() =>  window.location.reload(), 1700);
     })
     .catch(err => {
       console.log(err)
@@ -87,265 +100,271 @@ export function Subscribe() {
   } 
 
   return(
-    <Form {...form}>
-      <form 
-        onSubmit={form.handleSubmit(sendSubscribe)} 
-        className="w-full max-w-2xl py-4 px-5 md:py-6 md:px-7 space-y-4 rounded-xl shadow border bg-zinc-50"
-      >
-        <h1 className="text-xl font-bold leading-none">Preencha todos os campos corretamente</h1>
+    <div>
+      {isSubscriber ? (
+        <IsSubscriber/>
+      ) : (
+        <Form {...form}>
+          <form 
+            onSubmit={form.handleSubmit(sendSubscribe)} 
+            className="w-full max-w-2xl py-4 px-5 md:py-6 md:px-7 space-y-4 rounded-xl shadow border bg-zinc-50"
+          >
+            <h1 className="text-xl font-bold leading-none">Preencha todos os campos corretamente</h1>
 
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nome completo</FormLabel>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome completo</FormLabel>
 
-              <FormControl>
-                <Input 
-                  {...field}
-                  value={field.value ?? ''} 
-                  className="" 
-                  placeholder="Insira seu nome completo"
-                />
-              </FormControl>
+                  <FormControl>
+                    <Input 
+                      {...field}
+                      value={field.value ?? ''} 
+                      className="" 
+                      placeholder="Insira seu nome completo"
+                    />
+                  </FormControl>
 
-              <FormMessage/>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="age"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Idade</FormLabel>
-
-              <FormControl>
-                <Input 
-                  {...field}
-                  type="number"
-                  value={field.value ?? ''} 
-                  className="" 
-                  placeholder="Insira sua idade"
-                />
-              </FormControl>
-
-              <FormMessage/>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="phone"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Contato</FormLabel>
-
-              <FormControl>
-                <Input 
-                  {...field}
-                  type="number"
-                  value={field.value ?? ''} 
-                  className="" 
-                  placeholder="Insira seu contato (Whatsapp)"
-                />
-              </FormControl>
-
-              <FormMessage/>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-
-              <FormControl>
-                <Input 
-                  {...field}
-                  value={field.value ?? ''} 
-                  className="" 
-                  placeholder="Insira seu email"
-                />
-              </FormControl>
-
-              <FormMessage/>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="advecMember"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>É membro da ADVEC?</FormLabel>
-
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  className="flex flex-col"
-                >
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <RadioGroupItem value="Sim"/>
-                    </FormControl>
-                      
-                    <FormLabel className="relative -top-1">Sim</FormLabel>
-                  </FormItem>
-
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <RadioGroupItem value="Não"/>
-                    </FormControl>
-                      
-                    <FormLabel className="relative -top-1">Não</FormLabel>
-                  </FormItem>
-                </RadioGroup>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="isInTheGroup"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Já está no grupo do central no whatsapp?</FormLabel>
-
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  className="flex flex-col"
-                >
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <RadioGroupItem value="Sim"/>
-                    </FormControl>
-                      
-                    <FormLabel className="relative -top-1">Sim</FormLabel>
-                  </FormItem>
-
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <RadioGroupItem value="Não"/>
-                    </FormControl>
-                      
-                    <FormLabel className="relative -top-1">Não</FormLabel>
-                  </FormItem>
-                </RadioGroup>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="tshirtSize"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Qual será o tamanho da sua camiseta?</FormLabel>
-
-              <FormControl>
-                <Input 
-                  {...field}
-                  value={field.value ?? ''} 
-                  className="uppercase placeholder:capitalize" 
-                  placeholder="Tamanho"
-                />
-              </FormControl>
-
-              <FormMessage/>
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="department"
-          render={() => (
-            <FormItem>
-              <div>
-                <FormLabel>Qual/Quais departamento(s) você gostaria de ser voluntário?</FormLabel>
-
-                <FormDescription>
-                  Você pode servir no mínimo um e no máximo dois departamentos.
-                </FormDescription>
-              </div>
-
-              {departments.map(department => (
-                <FormField
-                  name="department"
-                  key={department.id}
-                  control={form.control}
-                  render={({ field }) => {
-                    const value = field.value || [];
-
-                    return (
-                      <FormItem 
-                        key={department.id} 
-                        className="flex items-center gap-2"
-                      >
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(department.id)}
-                            disabled={!value.includes(department.id) && value.length >= 2}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...value, department.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== department.id
-                                    )
-                                  )
-                            }}
-                          />
-                        </FormControl>
-
-                        <FormLabel className="relative -top-1 cursor-pointer">
-                          {department.label}
-                        </FormLabel>
-                      </FormItem>
-                    )
-                  }}
-                />
-              ))}
-            </FormItem>
-          )}
-        />
-
-        <div>
-          {isPast(finalDateSubscribe) ? (
-            <div className="w-full h-10 flex items-center justify-center text-white bg-red-500 rounded">
-              Inscrições encerradas!
-            </div>
-          ) : (
-            <Button
-              size="lg"
-              type="submit"
-              disabled={loading}
-              variant="destructive"
-              className="w-full font-bold gap-2 disabled:opacity-50"
-            >
-              {loading ? (
-                <Loader className="size-4 animate-spin" />
-              ) : (
-                <>
-                  Enviar Inscrição
-                  <ArrowRight className="size-4" />
-                </>
+                  <FormMessage/>
+                </FormItem>
               )}
-            </Button>
-          )}
-        </div>
-      </form>
-    </Form>
+            />
+
+            <FormField
+              control={form.control}
+              name="age"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Idade</FormLabel>
+
+                  <FormControl>
+                    <Input 
+                      {...field}
+                      type="number"
+                      value={field.value ?? ''} 
+                      className="" 
+                      placeholder="Insira sua idade"
+                    />
+                  </FormControl>
+
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contato</FormLabel>
+
+                  <FormControl>
+                    <Input 
+                      {...field}
+                      type="number"
+                      value={field.value ?? ''} 
+                      className="" 
+                      placeholder="Insira seu contato (Whatsapp)"
+                    />
+                  </FormControl>
+
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+
+                  <FormControl>
+                    <Input 
+                      {...field}
+                      value={field.value ?? ''} 
+                      className="" 
+                      placeholder="Insira seu email"
+                    />
+                  </FormControl>
+
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="advecMember"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>É membro da ADVEC?</FormLabel>
+
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      className="flex flex-col"
+                    >
+                      <FormItem className="flex items-center gap-2">
+                        <FormControl>
+                          <RadioGroupItem value="Sim"/>
+                        </FormControl>
+                          
+                        <FormLabel className="relative -top-1">Sim</FormLabel>
+                      </FormItem>
+
+                      <FormItem className="flex items-center gap-2">
+                        <FormControl>
+                          <RadioGroupItem value="Não"/>
+                        </FormControl>
+                          
+                        <FormLabel className="relative -top-1">Não</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="isInTheGroup"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Já está no grupo do central no whatsapp?</FormLabel>
+
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      className="flex flex-col"
+                    >
+                      <FormItem className="flex items-center gap-2">
+                        <FormControl>
+                          <RadioGroupItem value="Sim"/>
+                        </FormControl>
+                          
+                        <FormLabel className="relative -top-1">Sim</FormLabel>
+                      </FormItem>
+
+                      <FormItem className="flex items-center gap-2">
+                        <FormControl>
+                          <RadioGroupItem value="Não"/>
+                        </FormControl>
+                          
+                        <FormLabel className="relative -top-1">Não</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="tshirtSize"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Qual será o tamanho da sua camiseta?</FormLabel>
+
+                  <FormControl>
+                    <Input 
+                      {...field}
+                      value={field.value ?? ''} 
+                      className="uppercase placeholder:capitalize" 
+                      placeholder="Tamanho"
+                    />
+                  </FormControl>
+
+                  <FormMessage/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="department"
+              render={() => (
+                <FormItem>
+                  <div>
+                    <FormLabel>Qual/Quais departamento(s) você gostaria de ser voluntário?</FormLabel>
+
+                    <FormDescription>
+                      Você pode servir no mínimo um e no máximo dois departamentos.
+                    </FormDescription>
+                  </div>
+
+                  {departments.map(department => (
+                    <FormField
+                      name="department"
+                      key={department.id}
+                      control={form.control}
+                      render={({ field }) => {
+                        const value = field.value || [];
+
+                        return (
+                          <FormItem 
+                            key={department.id} 
+                            className="flex items-center gap-2"
+                          >
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value?.includes(department.id)}
+                                disabled={!value.includes(department.id) && value.length >= 2}
+                                onCheckedChange={(checked) => {
+                                  return checked
+                                    ? field.onChange([...value, department.id])
+                                    : field.onChange(
+                                        field.value?.filter(
+                                          (value) => value !== department.id
+                                        )
+                                      )
+                                }}
+                              />
+                            </FormControl>
+
+                            <FormLabel className="relative -top-1 cursor-pointer">
+                              {department.label}
+                            </FormLabel>
+                          </FormItem>
+                        )
+                      }}
+                    />
+                  ))}
+                </FormItem>
+              )}
+            />
+
+            <div>
+              {isPast(finalDateSubscribe) ? (
+                <div className="w-full h-10 flex items-center justify-center text-white bg-red-500 rounded">
+                  Inscrições encerradas!
+                </div>
+              ) : (
+                <Button
+                  size="lg"
+                  type="submit"
+                  disabled={loading}
+                  variant="destructive"
+                  className="w-full font-bold gap-2 disabled:opacity-50"
+                >
+                  {loading ? (
+                    <Loader className="size-4 animate-spin" />
+                  ) : (
+                    <>
+                      Enviar Inscrição
+                      <ArrowRight className="size-4" />
+                    </>
+                  )}
+                </Button>
+              )}
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
   );
 }
